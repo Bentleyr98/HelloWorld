8f2ac18e0d6401e1f88bdeeb4ffa44cc2226e9c5
= <bentley.rebekah314@gmail.com>
2022-04-19 13:32:57 -0600

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// This is a simple Java program.
+// Prints "Hello World!" to the terminal window.
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
